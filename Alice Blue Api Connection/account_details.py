fca2c75cdb0ae77c9341b825be26603d1099068b
debaonline4u <debashis.sahoo88@gmail.com>
2021-06-03 09:09:31 +0530

    account details updated

--- Alice Blue Api Connection/account_details.py
+++ Alice Blue Api Connection/account_details.py
@@ -1,7 +1,7 @@
-usr_name = '251492'
-pwd = 'Sadhguru@25'
-tfa = '8264'
-app_id = 'onwkx1XGMJ'
-api_scrt = 'boKXhd4fV6HI5LxBe0VD4kKci0mP6ZhTDeM5VRCA7HdL5kGHIPYyfV8EiP8aDBMZ'
+usr_name = 'your_aliceblue_username'
+pwd = 'your_aliceblue_password'
+tfa = 'your_two_fector_authentication@aliceblue'
+app_id = 'your_appid'
+api_scrt = 'your_api_secret_generated_at_creation_of_app_at_aliceblue'
 
 # redirect url = 'https://ant.aliceblueonline.com/plugin/callback'
