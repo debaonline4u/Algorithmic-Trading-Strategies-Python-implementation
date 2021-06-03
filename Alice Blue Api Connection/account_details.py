5280bd647d7c497305e0df4fe868f226248dc44a
debaonline4u <debashis.sahoo88@gmail.com>
2021-06-03 09:06:13 +0530

    Account details

---
+++ Alice Blue Api Connection/account_details.py
@@ -0,0 +1,7 @@
+usr_name = '251492'
+pwd = 'Sadhguru@25'
+tfa = '8264'
+app_id = 'onwkx1XGMJ'
+api_scrt = 'boKXhd4fV6HI5LxBe0VD4kKci0mP6ZhTDeM5VRCA7HdL5kGHIPYyfV8EiP8aDBMZ'
+
+# redirect url = 'https://ant.aliceblueonline.com/plugin/callback'
